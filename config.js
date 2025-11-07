87b9a51bff751762ffb1c68424af7ce3080680a8
arimakomi <126190829+arimakomi@users.noreply.github.com>
2025-11-07 21:16:11 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzK1Y1OzK2yMNil2ushkZOeqlB2dtdBTc9If1NAf2A5n2RJirY6wRaq6gNGot40BO1P/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwySHV6IlNpbx0clO_NdUf0IioQ2JdHH1vBlOtlGbqAN5NZFxOQAATWHt1RJa_TSt6x/exec",
   ADMIN_USERNAME: "admin",
   ADMIN_PASSWORD: "1234"
 };
