45171c10d8779b1f0811b19c98914c9f9f5b9dde
arimakomi <126190829+arimakomi@users.noreply.github.com>
2025-11-07 20:22:45 +0330

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 const CONFIG = {
     ADMIN_USERNAME: "admin",
     ADMIN_PASSWORD: "Yadbegir88"
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx.../exec"  // بعد از ایجاد اسکریپت جایگزین کنید
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxqy-fZAbkD8YUuI3H9CG35MulWZlBon2VYN2uYBPinvchh1_l6qpJtebxQKFLl0U43/exec"  // بعد از ایجاد اسکریپت جایگزین کنید
 };
