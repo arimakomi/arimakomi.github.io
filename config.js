65cd035dfd1d930052a1bf9d1bf62f04badbfc46
arimakomi <126190829+arimakomi@users.noreply.github.com>
2025-11-07 21:03:39 +0330

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 const CONFIG = {
     ADMIN_USERNAME: "admin",
     ADMIN_PASSWORD: "starly2025",  // تغییر دهید
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyAt4tT5XRQXLFAfyxlLLxsfJvGIQ7ji3-Yuw1OF8Nmm5uoKju88OzB3R8C6TlmILN6/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzK1Y1OzK2yMNil2ushkZOeqlB2dtdBTc9If1NAf2A5n2RJirY6wRaq6gNGot40BO1P/exec"
 };
