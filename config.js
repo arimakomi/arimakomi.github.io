fc69efcd722c49d09560ad45ba5d603391ca10c4
arimakomi <126190829+arimakomi@users.noreply.github.com>
2025-11-07 20:13:14 +0330

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    ADMIN_USERNAME: "admin",
+    ADMIN_PASSWORD: "Yadbegir88"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx.../exec"  // بعد از ایجاد اسکریپت جایگزین کنید
+};
