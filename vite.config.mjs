df7a5f92db2066f312430d172cda5d5741584079
arimakomi <126190829+arimakomi@users.noreply.github.com>
2025-11-09 22:04:42 +0330

    Delete vite.config.mjs

--- vite.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/starly-mini/'  // اگر دامنه سفارشی داری، آن را جایگزین کن
-});
